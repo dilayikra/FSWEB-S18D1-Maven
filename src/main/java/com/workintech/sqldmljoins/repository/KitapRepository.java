c4458d128f050a94f7c36ab07a3894732167cf44
dilayikra <119875314+dilayikra@users.noreply.github.com>
2026-03-23 17:33:24 +0300

    SQL Sorgu

--- src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
+++ src/main/java/com/workintech/sqldmljoins/repository/KitapRepository.java
@@ -8,15 +8,16 @@ import java.util.List;
 
 public interface KitapRepository extends JpaRepository<Kitap, Long> {
 
-    //Dram ve Hikaye türündeki kitapları listeleyin. JOIN kullanmadan yapın.
-    String QUESTION_1 = "";
+    // Dram ve Hikaye türündeki kitapları listeleyin. JOIN kullanmadan yapın.
+    // Mantık: Kitap tablosundaki turno sütununu, tur tablosundan gelen sonuçla filtreliyoruz.
+    String QUESTION_1 = "SELECT * FROM kitap WHERE turno IN (SELECT turno FROM tur WHERE ad IN ('Dram', 'Hikaye'))";
     @Query(value = QUESTION_1, nativeQuery = true)
     List<Kitap> findBooks();
 
-
-    String QUESTION_10 = "";
+    // Tüm kitapların ortalama puanını bulunuz.
+    // Mantık: AVG fonksiyonu ile puan sütununun ortalamasını alıyoruz.
+    String QUESTION_10 = "SELECT AVG(puan) FROM kitap";
     @Query(value = QUESTION_10, nativeQuery = true)
     Double findAvgPointOfBooks();
-
 
 }
